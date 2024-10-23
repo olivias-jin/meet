bedf4d164af8cb1b6f703cbd9fbc81de5af0add5
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-22 21:14:51 -0400

    static-site-test

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://olivias-jin.github.io/meet"
+  "https://olivias-jin.github.io/meet/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -13,6 +13,7 @@ const oAuth2Client = new google.auth.OAuth2(
   CLIENT_SECRET,
   redirect_uris[0]
 );
+
 
 module.exports.getAuthURL = async () => {
   /**
@@ -35,4 +36,33 @@ module.exports.getAuthURL = async () => {
       authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+  
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+  .then ((results) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials' :true,
+      },
+      body: JSON.stringify(results),
+    };
+  })
+  .catch((error) => {
+    return {
+      statusCode :500,
+      body: JSON.stringify(error),
+    };
+  });
 };
