4944ed8e44ff1368b264d4cb04ec4a0ee65e0bac
jsy5888 <oliviasunjin0318@gmail.com>
2024-10-26 00:45:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
+
 import './App.css';
 
-
 const App = () => {
+
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+
   return (
     <div className="App">
-        <CitySearch />
-        <EventList />
-  
-    </div>
-  );
+      <CitySearch />
+      <NumberOfEvents />
+      <EventList events={events} />
+
+    </div>);
+
 }
+
 
 export default App;
